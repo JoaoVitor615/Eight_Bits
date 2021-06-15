079a9bea96371d0a046a8c76fb2af9151809fdfa
João Vítor <joao.peniza@etec.sp.gov.br>
2021-06-14 22:43:49 -0300

    Criando a "activity_informacoes" e adicionando funções no botão "btnInfo".

--- app/src/main/java/com/example/eightbits/MainActivity.java
+++ app/src/main/java/com/example/eightbits/MainActivity.java
@@ -2,18 +2,31 @@ package com.example.eightbits;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageButton botaoAbrirInfo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        botaoAbrirInfo=findViewById(R.id.btnInfo);
+
+        botaoAbrirInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent telaInfo=new Intent(getApplicationContext(),Informacoes.class);
+                startActivity(telaInfo);
+            }
+        });
 
         getSupportActionBar().hide();
-
-        setContentView(R.layout.activity_main);
     }
 }
