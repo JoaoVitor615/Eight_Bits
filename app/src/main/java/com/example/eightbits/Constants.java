a4ed6c09b419e2e9d2691172b8d50289f3e04a9a
João Vítor <85712947+JoaoVitor615@users.noreply.github.com>
2021-09-30 09:55:43 -0300

    Update Constants.java

--- app/src/main/java/com/example/eightbits/Constants.java
+++ app/src/main/java/com/example/eightbits/Constants.java
@@ -4,10 +4,7 @@ package com.example.eightbits;
 
 public final class Constants {
 
-    public enum Type {
-        INTERNAL, EXTERNAL
-    }
-
+    public enum Type { INTERNAL, EXTERNAL}
     public static final String STORAGE_TYPE = "storage_type";
     public static final String FILE_NAME = "arquivo.txt";
 
